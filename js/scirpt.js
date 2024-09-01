eacbac6178c413326dc4d819a5224113470ceee9
ririn123456 <156205459+ririn123456@users.noreply.github.com>
2024-09-01 19:44:05 +0700

    Add files via upload

---
+++ js/scirpt.js
@@ -0,0 +1,5 @@
+// Toggle class active
+const navbarNav = document.querySelector("navbar-nav");
+document.querySelector("#humberger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
